e16a21a999e33159796f918846d4da8d2f58f305
Camilo Garzon <kmilogarzonk@hotmail.com>
2020-07-29 17:49:21 -0500

    clean

--- platforms/browser/www/js/config.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).bind("mobileinit", function() {
-    $nombre = device.name;
-    $cordova = device.cordova;
-    $sistoper = device.platform;
-    $direccion = device.uuid;
-    $version = device.version;
-    $('#propiedades').append('<ul><li>'+$nombre+'</li><li>'+$cordova+'</li><li>'+$sistoper+'</li><li>'+$direccion+'</li><li>'+$version+'</li></ul>');
-});
